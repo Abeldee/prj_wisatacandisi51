94a46ea4de507ba784e52854fadbdfcf77e84651
AbelDe <abeldlnd11@gmail.com>
2023-10-16 20:38:26 +0700

    candi

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:prj_wisatacandisi51/widgets/detail_screen.dart';
+import 'package:prj_wisatacandisi51/data/candi_data.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Candiu',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: DetailScreen(candi: candiList[0]),
+    );
+  }
+}
