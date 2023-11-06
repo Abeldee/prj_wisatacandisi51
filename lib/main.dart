4301712970efee69baa576b063a2a48d2d4ce30c
Abeldee <109330091+Abeldee@users.noreply.github.com>
2023-11-06 20:34:55 +0700

    tambah Profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:prj_wisatacandisi51/widgets/detail_screen.dart';
 import 'package:prj_wisatacandisi51/data/candi_data.dart';
+import 'package:prj_wisatacandisi51/widgets/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,14 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Wisata Candiu',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(candi: candiList[0]),
+      home: ProfileScreen(),
     );
   }
 }
