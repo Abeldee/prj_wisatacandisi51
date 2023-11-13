8a36a9583cee49c7961163ed2791d55b4722395e
Abeldee <109330091+Abeldee@users.noreply.github.com>
2023-11-13 20:36:34 +0700

    Search Screenn
    jahat onyet

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:prj_wisatacandisi51/widgets/detail_screen.dart';
 import 'package:prj_wisatacandisi51/data/candi_data.dart';
 import 'package:prj_wisatacandisi51/widgets/profile_screen.dart';
+import 'package:prj_wisatacandisi51/widgets/search_screen.dart';
+import 'package:prj_wisatacandisi51/widgets/signIn_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      title: 'Wisata Candiu',
+      title: 'Wisata Candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.deepPurple),
+          titleTextStyle: TextStyle(
+            color: Colors.deepPurple,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+          primary: Colors.deepPurple,
+          surface: Colors.deepPurple[50],
+        ),
       ),
-      home: ProfileScreen(),
+      home: SearchScreen(),
     );
   }
 }
